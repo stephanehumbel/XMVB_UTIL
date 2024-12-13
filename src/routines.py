5a07d53c902d2ec137fd64fae821feaa145a9a2f
Theo Rolain <theo.rolain@centrale-med.fr>
2024-12-13 16:14:28 +0100

    new modifs

--- src/routines.py
+++ src/routines.py
@@ -69,8 +69,6 @@ def Read_INTEGER(file_path, STRING, size):
         #print(STRING,':',col1,':::',num)
         return int(num)
 
-
-
 def detect_blank(file_path, start_line):
     '''
     Description: Get the position of the next blank line in the file.
@@ -104,14 +102,15 @@ def read_orb(file_name):
     with open(file_name, 'r') as file:
         for line in file:
             line = line.strip()
-            if line.startswith("#"):
-                om1 = True  # Starting processing data after the 1st "#"
-                if coef:  
+            if ("#" in line):
+                if om1:  
                     all_coeffs.append(coef)
                     all_aos.append(ao)
                     coef = []  
-                    current_aos = []
-            elif om1 and line:
+                    ao = []
+                else :
+                    om1 = True
+            elif om1 :
                 values = line.split()
                 for i in range(0, len(values), 2):
                     coef.append(float(values[i])) # add the last to vectors
@@ -216,48 +215,33 @@ def compte_AO(vect):
         print(len(tab_ao[i]),end=' ')   
     print()
     return tab_ao  
-        
-# read types in basis
+                  
 def reorder_OA(tab):
-   Ntab=len(tab)
-   reord=[]
-   for i in range(Ntab):
-       reord.append(i)
-   for i in range(Ntab):
-       if tab[i] == "XX":
+    reord = list(range(len(tab)))  # initial indices
+    for i in range(len(tab)):
+        if tab[i] == "XX" :
               #reord the 6 AO's 'XX', 'YY', 'ZZ', 'XY', 'XZ', 'YZ'
               # into            'XX', 'XY', 'XZ', 'YY', 'YZ', 'ZZ'
               #  0      1      2      3      4      5
-              #  0      3      4      1      5      2   
-              reord[i+1] = i+3   
-              reord[i+2] = i+4   
-              reord[i+3] = i+1   
-              reord[i+4] = i+5   
-              reord[i+5] = i+2   
-              i+=5
-          #    print(i)
-   return reord
-           
-        
-        
-        
+              #  0      3      4      1      5      2
+            reord[i + 1:i + 6] = [i + 3, i + 4, i + 1, i + 5, i + 2]
+            i+=5
+    return reord       
+               
 # read types in basis
 def read_basis(file_name):
-   num,line=detect_keyword(file_name, "OPTIMIZED ORBITALS", 0) 
-   with open(file_name, 'r') as file:
-        type=[]
-        line_num=0
-        for line in file:
-#            print(line[0:2])
-            values=[]
-            line_num+=1
-            if line_num >= num+6:
-              #  print(line_num, line,num)
-                if line=='\n':
+    num, _ = detect_keyword(file_name, "OPTIMIZED ORBITALS", 0)
+    types = []
+
+    with open(file_name, 'r') as file:
+        for line_num, line in enumerate(file, start=1):
+            if line_num >= num + 6:
+                if line.strip() == '':
                     break
-                values = re.split(' +|\n',line)
-                type.append(values[4])
-   return type
+                values = re.split(r'\s+', line.strip())
+                types.append(values[4])
+
+    return types
 
 # READ_geom
 def read_geom(file_name):
@@ -292,6 +276,7 @@ def read_geom(file_name):
                 z.append(float(values[5])*a0)   
 #    print('read_geom',x,y,z,natoms)
     return symbol,zat,x,y,z,natoms
+
 # READ_VEC
 def read_vec(file_path,vectors,start_line):
     '''
@@ -581,6 +566,7 @@ def write_gus(filename, phis,indices, deb, fin):
             f.write("\n$end \n")
             #for i in range(len(indices)):
 #    print("-end write_orbs-----") 
+
 def write_orb(filename, coeffs, indices, deb, fin):
 #    print('write_orb',filename,len(coeffs),len(indices))
     if filename == 'screen':
