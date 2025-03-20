8ff87fade0cbd7ac647a5e2e8379b08fd618d4ce
stephanehumbel <82397859+stephanehumbel@users.noreply.github.com>
2025-03-20 18:04:28 +0100

    Update CAS_LEW.py quick fix for reading nOM % 5 == 0 MCSCF Orb

--- src/CAS_LEW.py
+++ src/CAS_LEW.py
@@ -231,6 +231,13 @@ def read5OM_LOG(file_name,nOM,nOA,pos,fin):# read a file with 3 blank lines, +1 
   #  print('size= environ',  (fin-pos -5-3*(nblock-1))/nblock)
 
     #print('MO_CAS',nOM,nOA, pos,fin)
+    #truc sale pour lire multiple de 5  orbitales,
+    if nOM % 5 == 0 :
+            print(nOM%5,"quick and dirty fix")
+            nOM=nOM+1
+    #truc sale pour lire moins que 5 orbitales,
+    if nOM<5:
+            nOM=5
    
     MO_CAS=np.zeros((nOM,nOA))
     ioa=0
